d57c974bcaa4f2fd60623d93d1d001240e96664d
Thomas Guillouët <thomas.guillouet@outlook.com>
2023-03-12 19:38:13 +0100

    Fetching the opened windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use once_cell::sync::OnceCell;
+
+use crate::windows::WindowManager;
+
+mod windows;
+
+pub static mut WINDOW_MANAGER: OnceCell<WindowManager> = OnceCell::new();
+
 fn main() {
-    println!("Hello, world!");
+    let window_manager = WindowManager::default();
+    unsafe {
+        WINDOW_MANAGER
+            .set(window_manager)
+            .expect("Could not set the wm instance");
+    }
+
+    let list = WindowManager::global().fetch_opened_windows();
+    for (_, window) in list {
+        println!("Window {:?}", window);
+    }
 }
